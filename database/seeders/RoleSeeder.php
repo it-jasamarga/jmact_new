30fe6db0486d0f9db6d397c829def22698555528
MII <mii@MIIs-MacBook-Pro.local>
2022-01-23 22:22:10 +0700

    RoleSeeder

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -43,6 +43,10 @@ class RoleSeeder extends Seeder
             [
                 'name' => 'Inputer',
                 'guard_name' => 'web'
+            ],
+            [
+                'name' => 'Service Provider',
+                'guard_name' => 'web'
             ]
         ];
         DB::statement('SET FOREIGN_KEY_CHECKS=1;');
