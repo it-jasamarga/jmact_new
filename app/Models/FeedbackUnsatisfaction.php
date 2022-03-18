c3e2cb9213459217d1283449c0474604de8c048d
adhiriady <89894822+adhiriady@users.noreply.github.com>
2022-03-18 19:09:09 +0700

    model FeedbackUnsastifaction left behind from first push

---
+++ app/Models/FeedbackUnsatisfaction.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class FeedbackUnsatisfaction extends Model
+{
+    use HasFactory;
+}
